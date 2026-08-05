1571cb01f9ce17e86d9f7d3cdbce72f1b513738f
guilhermezi <guilherme.elinogueira@gmail.com>
2026-08-05 14:22:25 -0300

    terminei tudo só falta o front no init e coesão das classes

--- src/main/java/controle/frmTela.java
+++ src/main/java/controle/frmTela.java
@@ -1,11 +1,12 @@
 package controle;
 
 import conexao.Conexao;
+
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel; // para reconhecer a Jtable
 import java.sql.*; // Para reconhecer os comandos do SQL
 
-public class frmTela extends javax.swing.JFrame{
+public class frmTela extends javax.swing.JFrame {
     Conexao con_cliente;
 
     public frmTela() {
@@ -18,7 +19,7 @@ public class frmTela extends javax.swing.JFrame{
         tblClientes.setAutoCreateRowSorter(true);// ativa a classificação ordenada da tabela
     }
 
-    public void preencherTabela(){
+    public void preencherTabela() {
         tblClientes.getColumnModel().getColumn(0).setPreferredWidth(4);
         tblClientes.getColumnModel().getColumn(1).setPreferredWidth(150);
         tblClientes.getColumnModel().getColumn(2).setPreferredWidth(11);
@@ -30,7 +31,7 @@ public class frmTela extends javax.swing.JFrame{
 
         try {
             con_cliente.resultSet.beforeFirst();
-            while (con_cliente.resultSet.next()){
+            while (con_cliente.resultSet.next()) {
                 modelo.addRow(new Object[]{
                         con_cliente.resultSet.getString("cod"),
                         con_cliente.resultSet.getString("nome"),
@@ -39,83 +40,87 @@ public class frmTela extends javax.swing.JFrame{
                         con_cliente.resultSet.getString("email")
                 });
             }
-        }catch (SQLException erro){
-            JOptionPane.showMessageDialog(null,"\n Erro ao listar dados da tabela!! :\n "+erro,"Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "\n Erro ao listar dados da tabela!! :\n " + erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
-    public void posicionarRegistro(){
+    public void posicionarRegistro() {
         try {
             con_cliente.resultSet.first(); //posiciona na 1º registro da tabela
             mostrar_Dados(); // chama o metodo que irá buscar o dado da tabela
-        }catch (SQLException erro){
-            JOptionPane.showMessageDialog(null,"Não foi possível posicionar no primeiro registro: "+erro,"Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Não foi possível posicionar no primeiro registro: " + erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
-    public void mostrar_Dados(){
+    public void mostrar_Dados() {
         try {
             txtCod.setText(con_cliente.resultSet.getString("cod"));
             txtNome.setText(con_cliente.resultSet.getString("nome"));
             txtDtNasc.setText(con_cliente.resultSet.getString("dt_nasc"));
             txtTelefone.setText(con_cliente.resultSet.getString("telefone"));
             txtEmail.setText(con_cliente.resultSet.getString("email"));
-        }catch (SQLException erro){
-            JOptionPane.showMessageDialog(null,"Não localizou dados: "+erro,"Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Não localizou dados: " + erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
-    private void tblClientesMouseClied(java.awt.event.MouseEvent evt){
+    private void tblClientesMouseClied(java.awt.event.MouseEvent evt) {
         int linha_selecionada = tblClientes.getSelectedRow();
-        txtCod.setText(tblClientes.getValueAt(linha_selecionada,0).toString());
-        txtNome.setText(tblClientes.getValueAt(linha_selecionada,1).toString());
-        txtDtNasc.setText(tblClientes.getValueAt(linha_selecionada,2).toString());
-        txtTelefone.setText(tblClientes.getValueAt(linha_selecionada,3).toString());
-        txtEmail.setText(tblClientes.getValueAt(linha_selecionada,4).toString());
+        txtCod.setText(tblClientes.getValueAt(linha_selecionada, 0).toString());
+        txtNome.setText(tblClientes.getValueAt(linha_selecionada, 1).toString());
+        txtDtNasc.setText(tblClientes.getValueAt(linha_selecionada, 2).toString());
+        txtTelefone.setText(tblClientes.getValueAt(linha_selecionada, 3).toString());
+        txtEmail.setText(tblClientes.getValueAt(linha_selecionada, 4).toString());
     }
 
-    private void tblClientesKeyPressed(java.awt.event.KeyEvent evt){
+    private void tblClientesKeyPressed(java.awt.event.KeyEvent evt) {
         // evento que sincroniza a grid com as setas do teclado
         int linha_selecionada = tblClientes.getSelectedRow();
-        txtCod.setText(tblClientes.getValueAt(linha_selecionada,0).toString());
-        txtNome.setText(tblClientes.getValueAt(linha_selecionada,1).toString());
-        txtDtNasc.setText(tblClientes.getValueAt(linha_selecionada,2).toString());
-        txtTelefone.setText(tblClientes.getValueAt(linha_selecionada,3).toString());
-        txtEmail.setText(tblClientes.getValueAt(linha_selecionada,4).toString());
+        txtCod.setText(tblClientes.getValueAt(linha_selecionada, 0).toString());
+        txtNome.setText(tblClientes.getValueAt(linha_selecionada, 1).toString());
+        txtDtNasc.setText(tblClientes.getValueAt(linha_selecionada, 2).toString());
+        txtTelefone.setText(tblClientes.getValueAt(linha_selecionada, 3).toString());
+        txtEmail.setText(tblClientes.getValueAt(linha_selecionada, 4).toString());
     }
 
-    private void btnPrimeiroRegestroActionPerformed(java.awt.event.ActionEvent evt){
+    private void btnPrimeiroRegestroActionPerformed(java.awt.event.ActionEvent evt) {
         try {
             con_cliente.resultSet.first();
             mostrar_Dados();
-        }catch (SQLException erro){
+        } catch (SQLException erro) {
 
         }
     }
 
-    private void btnUltimoRegestroActionPerformed(java.awt.event.ActionEvent evt){
+    private void btnUltimoRegestroActionPerformed(java.awt.event.ActionEvent evt) {
         try {
             con_cliente.resultSet.last();
             mostrar_Dados();
-        }catch (SQLException erro){
+        } catch (SQLException erro) {
 
         }
     }
 
-    private void btnVoltarUmRegestroActionPerformed(java.awt.event.ActionEvent evt){
+    private void btnVoltarUmRegestroActionPerformed(java.awt.event.ActionEvent evt) {
         try {
-            con_cliente.resultSet.previous();
+            if (!con_cliente.resultSet.previous()) {
+                con_cliente.resultSet.first();
+            }
             mostrar_Dados();
-        }catch (SQLException erro){
+        } catch (SQLException erro) {
 
         }
     }
 
-    private void btnAvancarUmRegestroActionPerformed(java.awt.event.ActionEvent evt){
+    private void btnAvancarUmRegestroActionPerformed(java.awt.event.ActionEvent evt) {
         try {
-            con_cliente.resultSet.next();
+            if (!con_cliente.resultSet.next()) {
+                con_cliente.resultSet.last();
+            }
             mostrar_Dados();
-        }catch (SQLException erro){
+        } catch (SQLException erro) {
 
         }
     }
@@ -129,26 +134,95 @@ public class frmTela extends javax.swing.JFrame{
         txtCod.requestFocus(); // posiciona o cursor neste campo para digitação
     }
 
-    private void btnGravarActionPerformed(java.awt.event.ActionEvent evt){
+    private void btnGravarActionPerformed(java.awt.event.ActionEvent evt) {
+        String nome = txtNome.getText();
+        String data_nasc = txtDtNasc.getText();
+        String email = txtEmail.getText();
+        String telefone = txtTelefone.getText();
+
+        try {
+            String insert_sql = "insert into tbclientes (nome,telefone, email, dt_nasc) values ('" + nome + "','" + telefone + "','" + email + "','" + data_nasc + "')";
+            con_cliente.statement.executeUpdate(insert_sql);
+            JOptionPane.showMessageDialog(null, "Gravação realizada com sucesso!!", "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+            con_cliente.resultSet.first();
+            preencherTabela();
+            mostrar_Dados();
+        } catch (SQLException errosql) {
+            JOptionPane.showMessageDialog(null, "\n Erro na gravação :\n" + errosql, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private void btnAlterarActionPerformed(java.awt.event.ActionEvent evt) {
         String nome = txtNome.getText();
         String data_nasc = txtDtNasc.getText();
         String email = txtTelefone.getText();
         String telefone = txtTelefone.getText();
+        String sql = "";
+        String msg = "";
 
         try {
-            String insert_sql="insert into tbclientes (nome,telefone, email, dt_nasc) values ('" + nome + "','" + telefone + "','" + email + "','" + data_nasc + "')";
-            con_cliente.statement.executeUpdate(insert_sql);
-            JOptionPane.showMessageDialog(null, "Gravação realizada com sucesso!!","Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
+            if (txtCod.getText().equals("")) {
+                sql = "insert into tbclientes (nome,telefone, email, dt_nasc) values ('" + nome + "','" + telefone + "','" + email + "','" + data_nasc + "')";
+                msg = "Gravação de registro";
+            } else {
+                sql = "update tbclientes set nome='" + nome + "',telefone='" + telefone + "',email='" + email + "',dt_nasc='" + data_nasc + "' where cod='" + txtCod.getText() + "'";
+            }
+
+            con_cliente.statement.executeUpdate(sql);
+            JOptionPane.showMessageDialog(null, msg + "Gravação realizada com sucesso!!", "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+
+            con_cliente.executaSQL("select * from tbclientes order by cod");
             con_cliente.resultSet.first();
             preencherTabela();
             mostrar_Dados();
-        }catch (SQLException errosql){
-            JOptionPane.showMessageDialog(null, "\n Erro na gravação :\n"+errosql,"Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (SQLException errosql) {
+            JOptionPane.showMessageDialog(null, "\n Erro na gravação :\n" + errosql, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
+    private void btnExcluirActionPerformed(java.awt.event.ActionEvent evt) {
+        String sql = "";
+
+        try {
+            int resposta = JOptionPane.showConfirmDialog(rootPane, "Deseja excluir o registro: ", "Confirmar Exclusão", JOptionPane.YES_NO_OPTION);
+            if (resposta == JOptionPane.YES_OPTION) {
+                sql = "delete from tbclientes where cod='" + txtCod.getText() + "'";
+                con_cliente.statement.executeUpdate(sql);
+                con_cliente.executaSQL("select * from tbclientes order by cod");
+                con_cliente.resultSet.first();
+                preencherTabela();
+                posicionarRegistro();
+            } else {
+                JOptionPane.showMessageDialog(null,"Operação cancelada pelo usuario!!","Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
+            }
+
+        } catch (SQLException errosql) {
+            JOptionPane.showMessageDialog(null, "\n Erro na gravação :\n" + errosql, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private void txt_pesquisaKeyReleased(java.awt.event.KeyEvent evt){
+        try {
+            String pesquisa = "select * from tbclientes where nome like '" + txt_pesquisa.getText() + "%'";
+            con_cliente.executaSQL(pesquisa);
+
+            if (con_cliente.resultSet.first()){
+                preencherTabela();
+            }
+            else {
+                JOptionPane.showMessageDialog(null,"\n Não existe dados com este paramêtro!!","Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
+            }
+        }catch (SQLException errosql){
+            JOptionPane.showMessageDialog(null,"\n Os dados digitados não foram localizados!! :\n "+errosql,"Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+
+
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-
+        frmTela tela = new frmTela();
+        tela.setVisible(true);
     }
 }
